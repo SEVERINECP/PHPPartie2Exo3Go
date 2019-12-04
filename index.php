ede360a5ae3fe6dd5f8ec4e3214362511fdb1389
SEVERINECP <sevpicot@gmail.com>
2019-12-04 13:31:55 +0100

    PHPPartie2Exo3End

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$gender = 'Man';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHPPartie2Exo3</title>
+  </head>
+  <body>
+    <?php if($gender != 'Man'){ ?>
+      C'est un développeur !!!
+    <?php }else{ ?>
+      C'est une développeuse !!!
+    <?php } ?>
+  </body>
+</html>
